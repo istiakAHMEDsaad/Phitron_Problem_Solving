252c17f14ae84eac46d5ba691bacefcefdb82fb8
istiakAHMEDsaad <istik2020@gmail.com>
2025-05-16 23:31:13 +0600

    array & function with pointer in c

---
+++ Module_15_pointer/5_function_with_arr.c
@@ -0,0 +1,17 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+
+void func(int a[]) {
+  a[0] = 100;
+  a[1] = 200;
+}
+
+int main() {
+  int a[5] = {10, 20, 30, 40, 50};
+  func(a);
+  for (int i = 0; i < 5; i++) {
+    printf("%d ", a[i]);
+  }
+  return 0;
+}
