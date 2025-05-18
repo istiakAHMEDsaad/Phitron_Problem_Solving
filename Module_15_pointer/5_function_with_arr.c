0b294edd4149947b523253eb259b06c6a81bd360
istiakAHMEDsaad <istik2020@gmail.com>
2025-05-18 20:42:53 +0600

    func with string

--- Module_15_pointer/5_function_with_arr.c
+++ Module_15_pointer/5_function_with_arr.c
@@ -2,16 +2,19 @@
 #include <stdio.h>
 #include <string.h>
 
-void func(int a[]) {
-  a[0] = 100;
-  a[1] = 200;
+void func(int arr[], int n) {
+  for (int i = 0; i < n; i++) {
+    printf("%d", arr[i]);
+  }
 }
 
 int main() {
-  int a[5] = {10, 20, 30, 40, 50};
-  func(a);
-  for (int i = 0; i < 5; i++) {
-    printf("%d ", a[i]);
+  int n;
+  scanf("%d", &n);
+  int arr[n];
+  for (int i = 0; i < n; i++) {
+    scanf("%d", &arr[i]);
   }
+  func(arr, n);
   return 0;
 }
