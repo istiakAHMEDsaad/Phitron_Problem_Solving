71043c913c25eb88355e9d8552b5532695b0630f
istiakAHMEDsaad <istik2020@gmail.com>
2025-06-12 14:27:05 +0600

    cpp sting!

---
+++ Learn_CPP/Module_5_string_class/1_string.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  string s1 = "hello";
+  string s2 = "hello";
+  if (s1 == s2) {
+    cout << "same\n";
+  } else {
+    cout << "different!";
+  }
+  //   char s1[10] = "Hello";
+  //   char s2[10] = "Hello";
+  //   if (strcmp(s1, s2) == 0) {
+  //     cout << "same\n";
+  //   } else {
+  //     cout << "different\n";
+  //   }
+  //   strcpy(s, "Gello");
+  //   cout << s << endl;
+
+  return 0;
+}
