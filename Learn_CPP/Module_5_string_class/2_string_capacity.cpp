9b7287a4b8a7e0dc483194fec84ae80298810b0f
istiakAHMEDsaad <istik2020@gmail.com>
2025-07-24 23:59:22 +0600

    more string

---
+++ Learn_CPP/Module_5_string_class/2_string_capacity.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  string s = "Hellow World";
+  cout << "'" << s << "'" << " size is: " << s.size() << endl;
+  cout << s.max_size() << endl;
+  return 0;
+}
---
+++ Learn_CPP/Module_5_string_class/8_string_inp_with_space.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int x;
+  cin >> x;
+  string s;
+  cin.ignore();
+  //   cin >> s;
+  //   cin.getline(s, 100);
+  getline(cin, s);
+  cout << x << " " << s << endl;
+
+  return 0;
+}
